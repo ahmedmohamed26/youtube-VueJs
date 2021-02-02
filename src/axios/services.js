0e45c5b8fffea06627383c13841f22f502392936
ahmed mohamed <ahmedmohamed26.github.io>
2021-02-02 23:54:36 +0200

    change ap_key because not working

--- src/axios/services.js
+++ src/axios/services.js
@@ -1,7 +1,6 @@
 import axios from '@/axios';
 
-const key = 'AIzaSyD3dyzYOtGqiGwhXD1Cu6zn0Wmvuuejz7A'
-
+const key = 'AIzaSyByfiYGDSj1iobdGIl-o-Svmjt8S1HgJ6E';
 export async function getRelatedVideo(vedioId) {
     let response = await axios.get(`search?part=snippet&relatedToVideoId=${vedioId}&type=video&key=${key}`)
     return response;
@@ -12,13 +11,15 @@ export async function getChannels(channelId) {
     return response;
 }
 
-export async function getPlaylists(channelId) {
-    let response = await axios.get(`playlists?part=snippet%2CcontentDetails&channelId=${channelId}&maxResults=25&key=${key}`)
+
+
+export async function getPlaylists(channelId,pageSize) {
+    let response = await axios.get(`playlists?part=snippet%2CcontentDetails&channelId=${channelId}&maxResults=${pageSize}&key=${key}`)
     return response;
 }
 
-export async function getPlaylistItems(playlistId) {
-    let response = await axios.get(`playlistItems?part=snippet%2CcontentDetails&maxResults=25&playlistId=${playlistId}&key=${key}`)
+export async function getPlaylistItems(playlistId,pageSize) {
+    let response = await axios.get(`playlistItems?part=snippet%2CcontentDetails&maxResults=${pageSize}&playlistId=${playlistId}&key=${key}`)
     return response;
 }
 
@@ -26,3 +27,4 @@ export async function getvideo(videoId) {
     let response = await axios.get(`videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${key}`)
     return response;
 }
+
