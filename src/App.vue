ccc1d724efc8cfcdab1d9622dcc41daeda20c7c6
ahmed mohamed <ahmedmohamed26.github.io>
2021-02-02 14:08:39 +0200

    handle mixin file and add shared file

--- src/App.vue
+++ src/App.vue
@@ -13,36 +13,5 @@ export default {
 </script>
 
 <style lang="scss">
-body {
-	background-color: #f5f5f5;
-	width: 100%;
-}
-.container {
-	padding: 0 10%;
-}
-.row {
-	display: flex;
-	flex-direction: row;
-	flex-wrap: wrap;
-	width: 100%;
-}
-.column {
-	display: flex;
-	flex-direction: column;
-	flex-basis: 100%;
-	min-height: 100px;
-	width: 100%;
-}
-.mr{
-	margin-right: 10px;
-}
-.ml{
-	margin-left: 10px;
-}
-.pr{
-	padding-right: 10px;
-}
-.pl{
-	padding-left: 10px;
-}
+
 </style>
